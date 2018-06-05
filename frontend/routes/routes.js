51c6768f80f6941ebbd75220b4db0c7bf3c1a242
zixuanhu <zixuan@lclark.edu>
2018-06-04 20:32:46 -0700

    update repo

--- frontend/routes/routes.js
+++ frontend/routes/routes.js
@@ -2,22 +2,22 @@ import React from "react";
 import { Route, IndexRoute } from "react-router";
 import Root from "../components/Root";
 import HomepageContainer from "../components/homepage/HomepageContainer";
-import SignUpContainer from "../components/signup/SignUpContainer";
-import LoginContainer from "../components/login/LoginContainer";
+// import SignUpContainer from "../components/signup/SignUpContainer";
+// import LoginContainer from "../components/login/LoginContainer";
 
-import UserProfileContainer from "../components/userprofile/UserProfileContainer";
-import NewHomeContainer from "../components/homes/new/NewHomePageContainer";
-import IndexHomeContainer from "../components/homes/index/IndexHomePageContainer";
-import HomeDetailPageContainer from "../components/homes/detail/detail_index/HomeDetailPageContainer";
-import HomeEditPageContainer from "../components/homes/edit/HomeEditPageContainer";
-import ManageHomePageContainer from "../components/manage/ManageHomePageContainer";
-import IndexTripPageContainer from "../components/trips/index/IndexTripPageContainer";
-import DetailTripPageContainer from "../components/trips/detail/DetailTripPageContainer";
+// import UserProfileContainer from "../components/userprofile/UserProfileContainer";
+// import NewHomeContainer from "../components/homes/new/NewHomePageContainer";
+// import IndexHomeContainer from "../components/homes/index/IndexHomePageContainer";
+// import HomeDetailPageContainer from "../components/homes/detail/detail_index/HomeDetailPageContainer";
+// import HomeEditPageContainer from "../components/homes/edit/HomeEditPageContainer";
+// import ManageHomePageContainer from "../components/manage/ManageHomePageContainer";
+// import IndexTripPageContainer from "../components/trips/index/IndexTripPageContainer";
+// import DetailTripPageContainer from "../components/trips/detail/DetailTripPageContainer";
 
 export default (
     <Route path="/" component={Root}>
         <IndexRoute component={HomepageContainer} />
-        <Route path="/signup" component={SignUpContainer} />
+        {/* <Route path="/signup" component={SignUpContainer} />
         <Route path="/login" component={LoginContainer} />
         <Route
             path="/userprofile/:identifer"
@@ -29,6 +29,6 @@ export default (
         <Route path="/homes/:home_id/edit" component={HomeEditPageContainer} />
         <Route path="/manage/:user_id" component={ManageHomePageContainer} />
         <Route path="/trips/:user_id" components={IndexTripPageContainer} />
-        <Route path="/trip/:trip_id" components={DetailTripPageContainer} />
+        <Route path="/trip/:trip_id" components={DetailTripPageContainer} /> */}
     </Route>
 );
