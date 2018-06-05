c357e6fa05534b96ad0bdf89e92de6c418218536
zixuanhu <zixuan@lclark.edu>
2018-06-04 21:18:56 -0700

    update repo

--- frontend/routes/routes.js
+++ frontend/routes/routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, IndexRoute } from "react-router";
+import {Route, IndexRoute} from "react-router";
 import Root from "../components/Root";
 import HomepageContainer from "../components/homepage/HomepageContainer";
 import NewHomeContainer from "../components/homes/new/NewHomePageContainer";
@@ -11,23 +11,23 @@ import IndexTripPageContainer from "../components/trips/index/IndexTripPageConta
 import DetailTripPageContainer from "../components/trips/detail/DetailTripPageContainer";
 // import UserProfileContainer from "../components/userprofile/UserProfileContainer";
 // import SignUpContainer from "../components/signup/SignUpContainer";
-import LoginContainer from "../components/login/LoginContainer";
+import LoginContainer from "../components/login/LogInContainer";
 
 export default (
     <Route path="/" component={Root}>
-        <IndexRoute component={HomepageContainer} />
-        <Route path="/homes/new" component={NewHomeContainer} />
-        <Route path="/homes/:home_id" component={HomeDetailPageContainer} />
-        <Route path="/homes/:home_id/edit" component={HomeEditPageContainer} />
-        <Route path="/manage/:user_id" component={ManageHomePageContainer} />
-        <Route path="/homes" component={IndexHomeContainer} />
-        <Route path="/trips/:user_id" component={IndexTripPageContainer} />
-        <Route path="/trip/:trip_id" component={DetailTripPageContainer} />
+        <IndexRoute component={HomepageContainer}/>
+        <Route path="/homes/new" component={NewHomeContainer}/>
+        <Route path="/homes/:home_id" component={HomeDetailPageContainer}/>
+        <Route path="/homes/:home_id/edit" component={HomeEditPageContainer}/>
+        <Route path="/manage/:user_id" component={ManageHomePageContainer}/>
+        <Route path="/homes" component={IndexHomeContainer}/>
+        <Route path="/trips/:user_id" component={IndexTripPageContainer}/>
+        <Route path="/trip/:trip_id" component={DetailTripPageContainer}/>
         {/* <Route
             path="/userprofile/:identifer"
             component={UserProfileContainer}
         /> */}
-        <Route path="/login" component={LoginContainer} />
+        <Route path="/login" component={LoginContainer}/>
         {/* <Route path="/signup" component={SignUpContainer} /> */}
     </Route>
 );
