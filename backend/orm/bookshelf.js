1bbc8fca6ed1475ae0f4eeb66daec0a7702c8063
zixuanhu <zixuan@lclark.edu>
2018-06-04 20:01:50 -0700

    update databse connect

--- backend/orm/bookshelf.js
+++ backend/orm/bookshelf.js
@@ -1,10 +1,10 @@
-import knex from "knex";
+const environment = process.env.NODE_ENV || "development";
+import KnexConfig from "../../knexfile";
+const knexConfig = KnexConfig[environment];
+import Knex from "knex";
+const knex = Knex(knexConfig);
 import bookshelf from "bookshelf";
-import knexConfig from "../../knexfile";
-
-const ORM = bookshelf(knex(knexConfig.development));
-
+const ORM = bookshelf(knex);
 // Add this line wherever you create your bookshelf instance
-ORM.plugin('bookshelf-page');
-
+ORM.plugin("bookshelf-page");
 export default ORM;
