75c3d7f0ad6b882cd7f583a6367bce8217a06eb5
zixuanhu <zixuan@lclark.edu>
2018-06-04 20:40:21 -0700

    update repo

--- frontend/routes/routes.js
+++ frontend/routes/routes.js
@@ -2,15 +2,15 @@ import React from "react";
 import { Route, IndexRoute } from "react-router";
 import Root from "../components/Root";
 import HomepageContainer from "../components/homepage/HomepageContainer";
-// import SignUpContainer from "../components/signup/SignUpContainer";
-// import LoginContainer from "../components/login/LoginContainer";
-
-// import UserProfileContainer from "../components/userprofile/UserProfileContainer";
 import NewHomeContainer from "../components/homes/new/NewHomePageContainer";
-// import IndexHomeContainer from "../components/homes/index/IndexHomePageContainer";
 import HomeDetailPageContainer from "../components/homes/detail/detail_index/HomeDetailPageContainer";
 import HomeEditPageContainer from "../components/homes/edit/HomeEditPageContainer";
 import ManageHomePageContainer from "../components/manage/ManageHomePageContainer";
+import SignUpContainer from "../components/signup/SignUpContainer.js";
+import LoginContainer from "../components/login/LoginContainer.js";
+
+// import UserProfileContainer from "../components/userprofile/UserProfileContainer";
+// import IndexHomeContainer from "../components/homes/index/IndexHomePageContainer";
 // import IndexTripPageContainer from "../components/trips/index/IndexTripPageContainer";
 // import DetailTripPageContainer from "../components/trips/detail/DetailTripPageContainer";
 
@@ -21,8 +21,9 @@ export default (
         <Route path="/homes/:home_id" component={HomeDetailPageContainer} />
         <Route path="/homes/:home_id/edit" component={HomeEditPageContainer} />
         <Route path="/manage/:user_id" component={ManageHomePageContainer} />
-        {/* <Route path="/signup" component={SignUpContainer} />
         <Route path="/login" component={LoginContainer} />
+        <Route path="/signup" component={SignUpContainer} />
+        {/* 
         <Route
             path="/userprofile/:identifer"
             component={UserProfileContainer}
